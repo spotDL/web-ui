c232bccc92878dc0458ce45e507803c82ca504e1
kuba <xnetcat.dev@gmail.com>
2023-04-08 11:28:08 +0200

    added support for secure web sockets

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   PROTOCOL: process.env.PROTOCOL || window.location.protocol,
-  WS_PROTOCOL: process.env.WS_PROTOCOL || 'ws:',
+  WS_PROTOCOL: process.env.WS_PROTOCOL || window.location.protocol === 'https:' ? 'wss:' : 'ws:',
   BACKEND: process.env.BACKEND || window.location.hostname,
   PORT: process.env.PORT || window.location.port,
   WS_PORT: process.env.WS_PORT || window.location.port,

6eeb1d6923063421eb69414b0738d9e1c474a07f
phcreery <phcreery@gmail.com>
2024-02-23 21:07:18 -0600

    update news banner

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -44,26 +44,25 @@
 
       <SearchInput />
       <div role="alert" class="alert alert-info shadow-lg my-4">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="stroke-current flex-shrink-0 w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            ></path>
-          </svg>
-          <span>
-            NEW!: This interface now can download Song, Album, Artist and
-            Playlist. <br />
-            <span v-if="version < '4.2.1'"> This version is n </span>
-          </span>
-        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="stroke-current flex-shrink-0 w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <span>
+          This interface now can download a song or an entire album, artist, or
+          playlist.
+          <br />
+          <span v-if="version < '4.2.1'"> This version is n </span>
+        </span>
       </div>
     </div>
   </div>
